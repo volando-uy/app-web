af667b547e2ee951b425040c6f75d945bf25d582
nahuelutec <nahuel.martinez.a@estudiantes.utec.edu.uy>
2025-09-30 19:18:11 -0300

    Cambio en flightroute ahora es solo para consultar y ver que ruta de vuelo hay creada, creada pagina de ciudad y categoría, además también para crear una ruta de vuelo

---
+++ public/src/views/createCity&Category/createCity&Category.js
@@ -0,0 +1,22 @@
+// Inyectar footer dinámicamente
+function importFooter() {
+  fetch("../footer/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
+}
+
+if (document.getElementById("footer")) {
+  importFooter();
+}
+fetch("../header/header.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+
+    const script = document.createElement("script");
+    script.src = "../header/header.js";
+    script.onload = () => { initHeader(); };
+    document.body.appendChild(script);
+  });
